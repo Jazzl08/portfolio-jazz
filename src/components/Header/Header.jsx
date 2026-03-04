8e855ac9fc41123f32d5ef0196711751b2b84754
Jazz Lens <102624@glr.nl>
2026-03-04 21:41:16 +0000

    Update resume download link to point to new PDF file and add the resume file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -60,7 +60,7 @@ export default function Header({ onOpenNavigation }) {
                     </button>
                 </div>
 
-                <a href="/resume.pdf" download className="resume-btn">
+                <a href="/CV_Jazz_Lens_Resume.pdf" download className="resume-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
                     {translate('btn.resume')}
                 </a>
